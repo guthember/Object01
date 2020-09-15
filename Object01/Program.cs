5980ac2c7a236477ec130ff2b14cfd1cc28d440d
guthember <guthember@gmail.com>
2020-09-15 09:28:48 +0200

    Add project files.

---
+++ Object01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object01
+{
+    class Ember
+    {
+        public string nev;
+        public int eletkor;
+
+        public string Bemutatkozas()
+        {
+            return $"{nev} vagyok {eletkor} éves.";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember pisti = new Ember();
+            Ember eva = new Ember();
+
+            eva.nev = "Nagy Éva";
+            eva.eletkor = 18;
+
+            pisti.nev = "Nagy Pisti";
+            pisti.eletkor = 20;
+
+            Console.WriteLine(pisti.Bemutatkozas());
+            Console.WriteLine(eva.Bemutatkozas());
+
+            Console.ReadKey();
+        }
+    }
+}
