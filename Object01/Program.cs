9641995ee0f79ea01ad636343cb146f383e25d7a
guthember <guthember@gmail.com>
2020-09-15 11:22:18 +0200

    Kor(), Teglalap()

--- Object01/Program.cs
+++ Object01/Program.cs
@@ -6,31 +6,89 @@ using System.Threading.Tasks;
 
 namespace Object01
 {
-    class Ember
+    class Teglalap
     {
-        public string nev;
-        public int eletkor;
+        private int a;
+        private int b;
 
-        public string Bemutatkozas()
+
+        public Teglalap(int aOldal, int bOldal)
         {
-            return $"{nev} vagyok {eletkor} éves.";
+            a = aOldal;
+            b = bOldal;
+        }
+
+
+        public int Kerulet()
+        {
+            return 2 * (a + b);
+        }
+
+        public int Terulet() 
+        {
+            return a * b;
         }
     }
+
+    class Kor
+    {
+        private int r;
+
+        public Kor(int sugar)
+        {
+            if( sugar < 1)
+            {
+                r = 1;
+            }
+            else
+            {
+                r = sugar;
+            }
+        }
+
+        private double Kerulet()
+        {
+            return 2 * r * Math.PI;
+        }
+
+        private double Terulet()
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+
+        public void AdatokKiirasa() 
+        {
+            Console.WriteLine("Sugár: {0}",r);
+            Console.WriteLine("Terület: {0}", Terulet());
+            Console.WriteLine("Kerület: {0}", Kerulet());
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Ember pisti = new Ember();
-            Ember eva = new Ember();
+            int a, b;
 
-            eva.nev = "Nagy Éva";
-            eva.eletkor = 18;
+            Console.Write("Kérem az a oldalt: ");
+            a = int.Parse(Console.ReadLine());
 
-            pisti.nev = "Nagy Pisti";
-            pisti.eletkor = 20;
+            Console.Write("Kérem a b oldalt: ");
+            b = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(pisti.Bemutatkozas());
-            Console.WriteLine(eva.Bemutatkozas());
+            Teglalap t = new Teglalap(a, b);
+
+            Console.Write("Írja be a kör sugarát: ");
+            int sugar = int.Parse(Console.ReadLine());
+            Kor k = new Kor(sugar);
+
+            Console.WriteLine("Téglalap");
+            Console.WriteLine("Kerület: {0}", t.Kerulet());
+            Console.WriteLine("Terület: {0}", t.Terulet());
+
+            Console.WriteLine("Kör");
+            k.AdatokKiirasa();
+
 
             Console.ReadKey();
         }
